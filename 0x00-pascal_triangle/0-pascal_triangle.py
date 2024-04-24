0ab35e6c4ab6f16aca12c26719ba026e825a2373
Avellin003 <uavellinbonaparte@gmail.com>
2024-04-24 16:47:56 +0200

    updates

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """Pascal's Triangle"""
 
 def pascal_triangle(n):
@@ -8,7 +7,9 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+
     pascal = [[1]]
+
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
