e4290a316f14a017a210125bdd8f9601de0c3b53
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-01 18:14:01 +0200

    updates

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""canUnlockAll module:
+contains the function canUnlockAll"""
+def canUnlockAll(boxes):
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    return False
