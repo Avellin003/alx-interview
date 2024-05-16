d37121cbe9e2daa18203a6c55a6e356b8089184d
root <root@avellin.localdomain>
2024-05-16 07:28:50 +0200

    updates

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,37 @@
 #!/usr/bin/python3
+'''reads stdin and prints key metrics'''
 
-"""Write a script that reads stdin line
-by line and computes metrics
-"""
+
 import sys
-import signal
 
+cached = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+size = 0
+counter = 0
 
-# Variables to store metrics
-total_file_size = 0
-status_counts = {}
-
-
-# Function to print statistics
-def print_statistics():
-    print("Total file size:", total_file_size)
-    for status_c in sorted(status_counts.keys()):
-        print(f"{status_code}: {status_counts[status_code]}")
-
-
-# Function to handle keyboard interruption
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
-
-
-# Register signal handler for keyboard interruption
-signal.signal(signal.SIGINT, signal_handler)
-
-# Read stdin line by line
 try:
-    for i, line in enumerate(sys.stdin, start=1):
-        parts = line.split()
-        if len(parts) == 7:
-            try:
-                status_code = int(parts[4])
-                file_size = int(parts[5])
-                total_file_size += file_size
-                status_counts[status_c] = status_counts.get(status_c, 0) + 1
-            except ValueError:
-                pass  # Skip line if status code or file size is not an integer
-        if i % 10 == 0:
-            print_statistics()
-except KeyboardInterrupt:  # Handle keyboard interruption
-    print_statistics()
+    for i in sys.stdin:
+        lllist = i.split(" ")
+        if len(lllist) > 4:
+            cmd = lllist[-2]
+            s = int(lllist[-1])
+            if cmd in cached.keys():
+                cached[cmd] += 1
+            size += s
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(size))
+            for key, value in sorted(cached.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(size))
+    for key, value in sorted(cached.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
