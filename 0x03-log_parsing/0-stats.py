c1e55bd89e80d805e466f8f5ab2f00b2725117b5
root <root@avellin.localdomain>
2024-05-16 20:47:58 +0200

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,43 +3,35 @@
 
 import sys
 
+if __name__ == '__main__':
 
-class LogParser:
-    def __init__(self):
-        self.filesize = 0
-        self.count = 0
-        self.codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-        self.stats = {k: 0 for k in self.codes}
+    fsize, counter = 0, 0
+    cds = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    status = {k: 0 for k in cds}
 
-    def print_stats(self):
-        print("File size: {:d}".format(self.filesize))
-        for k, v in sorted(self.stats.items()):
+    def print_status(status: dict, fsize: int) -> None:
+        print("File size: {:d}".format(fsize))
+        for k, v in sorted(status.items()):
             if v:
                 print("{}: {}".format(k, v))
 
-    def parse_logs(self):
-        try:
-            for line in sys.stdin:
-                self.count += 1
-                data = line.split()
-                try:
-                    status_code = data[-2]
-                    if status_code in self.stats:
-                        self.stats[status_code] += 1
-                except Exception:
-                    pass
-                try:
-                    self.filesize += int(data[-1])
-                except Exception:
-                    pass
-                if self.count % 10 == 0:
-                    self.print_stats()
-            self.print_stats()
-        except KeyboardInterrupt:
-            self.print_stats()
-            raise
-
-
-if __name__ == '__main__':
-    parser = LogParser()
-    parser.parse_logs()
+    try:
+        for lne in sys.stdin:
+            counter += 1
+            data = lne.split()
+            try:
+                scode = data[-2]
+                if scode in status:
+                    status[scode] += 1
+            except BaseException:
+                pass
+            try:
+                fsize += int(data[-1])
+            except BaseException:
+                pass
+            if counter % 10 == 0:
+                print_status(status, fsize)
+        print_status(status, fsize)
+    except KeyboardInterrupt:
+        print_status(status, fsize)
+        raise
