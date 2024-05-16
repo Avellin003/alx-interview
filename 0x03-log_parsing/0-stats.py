2dd2dfbf3b78aee61b10cf66a5ef6b452f0ef0d8
root <root@avellin.localdomain>
2024-05-16 07:45:09 +0200

    task_0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,66 @@
 #!/usr/bin/python3
-'''reads stdin and prints key metrics'''
+"""Script to calculate metrics by reading stdin line by line:
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size> (line is skipped if not in this format)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+prints statistics:
+Total file size: File size: <tsize>
+where <tsize> is the sum of all previous <file size>
+(see input format above)
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405, and 500
+if a status code doesn’t appear or is not an integer,
+no output for that status code
+format: <status code>: <number>
+status codes printed in ascending order
+
+line_info = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
+<status code>, <file size>]
+"""
 
 import sys
 
-cached = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
-size = 0
-counter = 0
+# Store the count of all status codes in a dictionary
+status_counts = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                 '404': 0, '405': 0, '500': 0}
+
+tsize = 0
+line_count = 0  # keep count of the number of lines processed
 
 try:
-    for i in sys.stdin:
-        lllist = i.split(" ")
-        if len(lllist) > 4:
-            cmd = lllist[-2]
-            s = int(lllist[-1])
-            if cmd in cached.keys():
-                cached[cmd] += 1
-            size += s
-            counter += 1
+    for line in sys.stdin:
+        line_info = line.split(" ")
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(size))
-            for key, value in sorted(cached.items()):
+        if len(line_info) > 4:
+            status_code = line_info[-2]
+            file_size = int(line_info[-1])
+
+            # Check if the status code exists in the dictionary and
+            # increment its count
+            if status_code in status_counts.keys():
+                status_counts[status_code] += 1
+
+            # Update tsize
+            tsize += file_size
+
+            # Update count of lines
+            line_count += 1
+
+        if line_count == 10:
+            line_count = 0  # Reset count
+            print('File size: {}'.format(tsize))
+
+            # Print out status code counts
+            for key, value in sorted(status_counts.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
-except Exception as err:
+except KeyboardInterrupt:
     pass
 
 finally:
-    print('File size: {}'.format(size))
-    for key, value in sorted(cached.items()):
+    print('File size: {}'.format(tsize))
+    for key, value in sorted(status_counts.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
